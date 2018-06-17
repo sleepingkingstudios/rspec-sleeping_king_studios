1c57e421408cf1142665257bba5560d7180bdfe3
Rob Smith <merlin@sleepingkingstudios.com>
2018-06-17 15:53:56 -0400

    Implement RSpec::SleepingKingStudios.gem_path.

--- lib/rspec/sleeping_king_studios.rb
+++ lib/rspec/sleeping_king_studios.rb
@@ -5,6 +5,13 @@ require 'rspec/core'
 module RSpec
   # Hic Iacet Arthurus, Rex Quondam, Rexque Futurus.
   module SleepingKingStudios
+    def self.gem_path
+      pattern =
+        /#{File::SEPARATOR}lib#{File::SEPARATOR}rspec#{File::SEPARATOR}?\z/
+
+      __dir__.sub(pattern, '')
+    end
+
     def self.version
       @version ||= RSpec::SleepingKingStudios::Version.to_gem_version
     end # class method version
--- spec/rspec/sleeping_king_studios_spec.rb
+++ spec/rspec/sleeping_king_studios_spec.rb
@@ -1,8 +1,27 @@
 # spec/rspec/sleeping_king_studios_spec.rb
 
+require 'spec_helper'
+
 require 'rspec/sleeping_king_studios'
 
 RSpec.describe RSpec::SleepingKingStudios do
+  describe '::gem_path' do
+    let(:expected) do
+      pattern =
+        /#{File::SEPARATOR}spec#{File::SEPARATOR}rspec#{File::SEPARATOR}?\z/
+
+      __dir__.sub(pattern, '')
+    end
+
+    it 'should define the class reader' do
+      expect(described_class).to respond_to(:gem_path).with(0).arguments
+    end
+
+    it 'should return the path to the gem root directory' do
+      expect(described_class.gem_path).to be == expected
+    end
+  end
+
   describe '::version' do
     let(:expected) do
       RSpec::SleepingKingStudios::Version.to_gem_version
