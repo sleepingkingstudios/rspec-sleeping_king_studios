a8122dd11ba93ed5f0d7c314034a99ffbcdfa36e
Rob Smith <merlin@sleepingkingstudios.com>
2018-06-26 01:42:32 -0400

    Implement RSpec::SleepingKingStudios::Support::ValueObservation.

---
+++ lib/rspec/sleeping_king_studios/support/value_observation.rb
@@ -0,0 +1,68 @@
+require 'rspec/sleeping_king_studios/support'
+
+module RSpec::SleepingKingStudios::Support
+  # Encapsulates the value of a method call or block, and captures a snapshot of
+  # the value when the observation is initialized for later comparison.
+  #
+  # @example Observing a Method
+  #   user  = Person.new(name: 'Alan Bradley')
+  #   value = ValueObservation.new(user, :name)
+  #   value.initial_value #=> 'Alan Bradley'
+  #   value.current_value #=> 'Alan Bradley'
+  #
+  #   user.name = 'Ed Dillinger'
+  #   value.initial_value #=> 'Alan Bradley'
+  #   value.current_value #=> 'Ed Dillinger'
+  #
+  # @example Observing a Block
+  #   value = ValueObservation.new { Person.where(virtual: false).count }
+  #   value.initial_value #=> 4
+  #   value.current_value #=> 4
+  #
+  #   Person.where(name: 'Kevin Flynn').enter_grid!
+  #   value.initial_value #=> 4
+  #   value.current_value #=> 3
+  class ValueObservation
+    # @overload initialize(object, method_name)
+    #   @param [Object] object The object to observe.
+    #
+    #   @param [Symbol, String] method_name The name of the method to observe.
+    #
+    # @overload initialize(&block)
+    #   @yield The value to observe. The block will be called each time the
+    #     value is requested, and the return value of the block will be given as
+    #     the current value.
+    def initialize(object = nil, method_name = nil, &block)
+      @observation = if block_given?
+        block
+      else
+        @method_name = method_name
+
+        -> { object.send(method_name) }
+      end
+
+      @initial_value = current_value
+    end
+
+    # @return [Object] the observed value at the time the observation was
+    #   initialized.
+    attr_reader :initial_value
+
+    def changed?
+      !RSpec::Support::FuzzyMatcher.values_match?(initial_value, current_value)
+    end
+
+    # @return [Object] the observed value when #current_value is called.
+    def current_value
+      @observation.call
+    end
+
+    # @return [String] a human-readable representation of the observed method
+    #   or block.
+    def description
+      return 'result' unless @method_name
+
+      @method_name.to_s
+    end
+  end
+end
---
+++ spec/rspec/sleeping_king_studios/support/value_observation_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+require 'rspec/sleeping_king_studios/concerns/wrap_examples'
+require 'rspec/sleeping_king_studios/matchers/core/construct'
+require 'rspec/sleeping_king_studios/matchers/core/have_predicate'
+require 'rspec/sleeping_king_studios/matchers/core/have_reader'
+
+require 'rspec/sleeping_king_studios/support/value_observation'
+
+RSpec.describe RSpec::SleepingKingStudios::Support::ValueObservation do
+  extend RSpec::SleepingKingStudios::Concerns::WrapExamples
+
+  shared_context 'when the value has changed' do
+    let(:changed_value) { 'changed value'.freeze }
+
+    before(:example) do
+      instance # Force evaluation of the memoized helper.
+
+      object.value = changed_value
+    end
+  end
+
+  shared_examples 'when the observation is defined with a block' do
+    let(:instance) { described_class.new { object.value.upcase } }
+  end
+
+  subject(:instance) { described_class.new(object, method_name) }
+
+  let(:method_name)   { :value }
+  let(:initial_value) { 'initial value'.freeze }
+  let(:object)        { Struct.new(method_name).new(initial_value) }
+
+  describe '::new' do
+    it 'should define the constructor' do
+      expect(described_class)
+        .to be_constructible
+        .with(0..2).arguments
+        .and_a_block
+    end
+  end
+
+  describe '#changed?' do
+    it { expect(instance).to have_predicate(:changed?).with_value(false) }
+
+    wrap_context 'when the value has changed' do
+      it { expect(instance.changed?).to be true }
+    end
+  end
+
+  describe '#current_value' do
+    it { expect(instance).to respond_to(:current_value).with(0).arguments }
+
+    it { expect(instance.current_value).to be initial_value }
+
+    wrap_context 'when the value has changed' do
+      it { expect(instance.current_value).to be changed_value }
+    end
+
+    wrap_examples 'when the observation is defined with a block' do
+      it { expect(instance.current_value).to be == initial_value.upcase }
+
+      wrap_context 'when the value has changed' do
+        it { expect(instance.current_value).to be == changed_value.upcase }
+      end
+    end
+  end
+
+  describe '#description' do
+    it { expect(instance).to have_reader(:description).with(method_name.to_s) }
+
+    wrap_examples 'when the observation is defined with a block' do
+      it { expect(instance.description).to be == 'result' }
+    end
+  end
+
+  describe '#initial_value' do
+    it { expect(instance).to respond_to(:initial_value).with(0).arguments }
+
+    it { expect(instance.initial_value).to be initial_value }
+
+    wrap_context 'when the value has changed' do
+      it { expect(instance.initial_value).to be initial_value }
+    end
+
+    wrap_examples 'when the observation is defined with a block' do
+      it { expect(instance.initial_value).to be == initial_value.upcase }
+
+      wrap_context 'when the value has changed' do
+        it { expect(instance.initial_value).to be == initial_value.upcase }
+      end
+    end
+  end
+end
