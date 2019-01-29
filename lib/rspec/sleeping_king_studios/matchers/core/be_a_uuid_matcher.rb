76ea829de625dbb4db88b8b6c9657ebeeb60bd93
Rob Smith <merlin@sleepingkingstudios.com>
2019-01-29 16:36:41 -0500

    Implement Matchers::Core::BeAUuid.

---
+++ lib/rspec/sleeping_king_studios/matchers/core/be_a_uuid_matcher.rb
@@ -0,0 +1,81 @@
+# frozen_string_literals: true
+
+require 'rspec/sleeping_king_studios/matchers/base_matcher'
+require 'rspec/sleeping_king_studios/matchers/core'
+
+module RSpec::SleepingKingStudios::Matchers::Core
+  # Matcher for testing whether an object is a UUID string.
+  #
+  # @since 2.5.0
+  class BeAUuidMatcher < RSpec::SleepingKingStudios::Matchers::BaseMatcher
+    # (see BaseMatcher#description)
+    def description
+      'be a UUID'
+    end
+
+    # (see BaseMatcher#failure_message)
+    def failure_message
+      message = super() + ', but '
+
+      return message + 'was not a String' unless string?
+
+      return message + 'was too short' if too_short?
+
+      return message + 'was too long' if too_long?
+
+      return message + 'has invalid characters' if invalid_characters?
+
+      return message + 'the format is invalid' unless valid_format?
+
+      message
+    end
+
+    # Checks if the object is a UUID string.
+    #
+    # @param [Object] actual The object to check.
+    #
+    # @return [Boolean] true if the object is a string with the correct format;
+    #   otherwise false.
+    def matches?(actual)
+      super
+
+      string? && valid_length? && valid_characters? && valid_format?
+    end
+
+    private
+
+    def invalid_characters?
+      @invalid_characters ||= @actual.match?(/[^A-Fa-f0-9\-]/)
+    end
+
+    def string?
+      @string ||= @actual.is_a?(String)
+    end
+
+    def too_long?
+      @too_long ||= @actual.length > 36
+    end
+
+    def too_short?
+      @too_short ||= @actual.length < 36
+    end
+
+    def uuid_format
+      chars = '[A-Fa-f0-9\-]'
+
+      /\A#{chars}{8}-#{chars}{4}-#{chars}{4}-#{chars}{4}-#{chars}{12}\z/
+    end
+
+    def valid_characters?
+      !invalid_characters?
+    end
+
+    def valid_format?
+      @valid_format || @actual.match?(uuid_format)
+    end
+
+    def valid_length?
+      !too_short? && !too_long?
+    end
+  end
+end
---
+++ spec/rspec/sleeping_king_studios/matchers/core/be_a_uuid_matcher_spec.rb
@@ -0,0 +1,114 @@
+# frozen_string_literals: true
+
+require 'spec_helper'
+require 'rspec/sleeping_king_studios/examples/rspec_matcher_examples'
+
+require 'rspec/sleeping_king_studios/matchers/core/be_a_uuid_matcher'
+
+RSpec.describe RSpec::SleepingKingStudios::Matchers::Core::BeAUuidMatcher do
+  include RSpec::SleepingKingStudios::Examples::RSpecMatcherExamples
+
+  let(:instance) { described_class.new }
+
+  describe '#description' do
+    it { expect(instance).to respond_to(:description).with(0).arguments }
+
+    it { expect(instance.description).to be == 'be a UUID' }
+  end
+
+  describe '#matches?' do
+    let(:failure_message) do
+      "expected #{actual.inspect} to be a UUID"
+    end
+    let(:failure_message_when_negated) do
+      "expected #{actual.inspect} not to be a UUID"
+    end
+
+    describe 'with nil' do
+      let(:failure_message) { super() << ', but was not a String' }
+      let(:actual)          { nil }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with a non Boolean object' do
+      let(:failure_message) { super() << ', but was not a String' }
+      let(:actual)          { Object.new }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with an empty string' do
+      let(:failure_message) { super() << ', but was too short' }
+      let(:actual)          { '' }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with a string that is too short' do
+      let(:failure_message) { super() << ', but was too short' }
+      let(:actual)          { '00000000' }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with a string that is too long' do
+      let(:failure_message) { super() << ', but was too long' }
+      let(:actual)          { '00000000-0000-0000-0000-000000000000-00000000' }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with a string with invalid characters' do
+      let(:failure_message) { super() << ', but has invalid characters' }
+      let(:actual)          { '01234567-89ab-cdef-ghij-klmnopqrstuv' }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with a string with invalid format' do
+      let(:failure_message) { super() << ', but the format is invalid' }
+      let(:actual)          { '0123456789abcdef0123456789abcdef----' }
+
+      include_examples 'should fail with a positive expectation'
+
+      include_examples 'should pass with a negative expectation'
+    end
+
+    describe 'with a numeric UUID' do
+      let(:actual) { '00000000-0000-0000-0000-000000000000' }
+
+      include_examples 'should pass with a positive expectation'
+
+      include_examples 'should fail with a negative expectation'
+    end
+
+    describe 'with an uppercase UUID' do
+      let(:actual) { '01234567-89AB-CDEF-0123-456789ABCDEF' }
+
+      include_examples 'should pass with a positive expectation'
+
+      include_examples 'should fail with a negative expectation'
+    end
+
+    describe 'with a lowercase UUID' do
+      let(:actual) { '01234567-89ab-cdef-0123-456789abcdef' }
+
+      include_examples 'should pass with a positive expectation'
+
+      include_examples 'should fail with a negative expectation'
+    end
+  end
+end
