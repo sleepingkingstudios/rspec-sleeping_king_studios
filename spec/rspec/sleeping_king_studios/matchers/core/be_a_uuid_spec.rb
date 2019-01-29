e93e241c3016044a9a6087a7268aed6ffd09a346
Rob Smith <merlin@sleepingkingstudios.com>
2019-01-29 17:46:49 -0500

    Implement Matchers::Macros#be_a_uuid.

---
+++ spec/rspec/sleeping_king_studios/matchers/core/be_a_uuid_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literals: true
+
+require 'spec_helper'
+
+require 'rspec/sleeping_king_studios/matchers/core/be_a_uuid'
+
+RSpec.describe RSpec::SleepingKingStudios::Matchers::Macros do
+  let(:matcher_class) do
+    RSpec::SleepingKingStudios::Matchers::Core::BeAUuidMatcher
+  end
+  let(:example_group) { self }
+
+  describe '#a_uuid' do
+    let(:matcher) { example_group.a_uuid }
+
+    it { expect(example_group).to respond_to(:a_uuid).with(0).arguments }
+
+    it { expect(matcher).to be_a RSpec::Matchers::AliasedMatcher }
+
+    it { expect(matcher.base_matcher).to be_a matcher_class }
+
+    it 'should have a custom description' do
+      expect(matcher.description).to be == 'a UUID'
+    end
+  end
+
+  describe '#be_a_uuid' do
+    let(:matcher) { example_group.be_a_uuid }
+
+    it { expect(example_group).to respond_to(:be_a_uuid).with(0).arguments }
+
+    it { expect(matcher).to be_a matcher_class }
+  end
+end
