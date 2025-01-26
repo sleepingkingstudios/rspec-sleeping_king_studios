2bb9a804a90fb79f1d714aadadef37706af7bfa1
Rob Smith <sleepingkingstudios@gmail.com>
2025-01-26 02:16:31 -0500

    Define Deferred::Calls::ExampleGroup#deferred_example_group.

--- spec/rspec/sleeping_king_studios/deferred/calls/example_group_spec.rb
+++ spec/rspec/sleeping_king_studios/deferred/calls/example_group_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rspec/sleeping_king_studios/concerns/example_constants'
 require 'rspec/sleeping_king_studios/deferred/calls/example_group'
+require 'rspec/sleeping_king_studios/deferred/examples'
 
 require 'support/shared_examples/deferred_call_examples'
 
@@ -24,4 +25,27 @@ RSpec.describe RSpec::SleepingKingStudios::Deferred::Calls::ExampleGroup do
   end
 
   include_examples 'should be a deferred call'
+
+  describe '#deferred_example_group' do
+    it 'should define the reader' do
+      expect(deferred).to respond_to(:deferred_example_group).with(0).arguments
+    end
+
+    it { expect(deferred.deferred_example_group).to be nil }
+
+    context 'when initialized with deferred_example_group: value' do
+      let(:deferred_example_group) do
+        Module.new do
+          include RSpec::SleepingKingStudios::Deferred::Examples
+        end
+      end
+      let(:keywords) do
+        super().merge(deferred_example_group: deferred_example_group)
+      end
+
+      it 'should return the configured value' do
+        expect(deferred.deferred_example_group).to be deferred_example_group
+      end
+    end
+  end
 end
